e81fa708d45001d8643b45db701c402b4cc74205
threecourse <daisukeka@gmail.com>
2020-11-24 02:24:09 +0900

    added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 
 module.exports = {
-  assetPrefix: process.env.GITHUB_PAGES ? '/repository-name' : ''
+  assetPrefix: process.env.GITHUB_PAGES ? '/visualize-8-queen-next' : ''
 }
